23fdbc7f315cb85f5a5a0a1611ff0478b872e2b9
Matteo Pulcini <matteo.pulcini19@gmail.com>
2022-02-09 13:20:01 -0500

    store measure + notes!

---
+++ src/main/java/store/Record.java
@@ -0,0 +1,19 @@
+package store;
+
+public class Record {
+	private int measureNumber;
+	private String notes;
+	
+	public Record() {
+		/* 1. Parse music XML
+		 * 2. Store basic attributes
+		 */
+		
+		
+	}
+	
+	public void storeMeasureNumber() {
+		
+	}
+	
+}
