c8dea3f295812efefbea05439388d26d5f3c77c5
Matteo Pulcini <matteo.pulcini19@gmail.com>
2022-04-07 13:44:20 -0400

    bars -> submission ready

--- src/main/java/drawMusic/DrawBars.java
+++ src/main/java/drawMusic/DrawBars.java
@@ -16,6 +16,8 @@ public class DrawBars {
 	}
 
 	public void drawbars() {
+		int counter16 = 0;
+		int counterQuarterEight = 0;
 		for(int j = 0; j < aLGuitar.size(); j++) {
 			GuitarInformation lg = aLGuitar.get(j);
 			int y = getMeasureYLocation(lg.getMeasure());
@@ -23,9 +25,20 @@ public class DrawBars {
 			if(j+1 < this.aLGuitar.size()) {
 				GuitarInformation g = null;
 				if(this.aLGuitar.get(j+1) != null) {g = this.aLGuitar.get(j+1);}
-				if(lg.getType().equals("16th") && lg.getMeasure() == g.getMeasure()) {
+				if(lg.getMeasure() == g.getMeasure()) {
+				if(lg.getType().equals("16th") && counter16 < 3 ) {
 					Line b = new Line(lg.getNoteX(), y, g.getNoteX(), y);
 					p.getChildren().add(b);
+					counter16++;
+
+				} else if((lg.getType().equals("quarter") || lg.getType().equals("eighth")) && counterQuarterEight < 1){
+					Line b = new Line(lg.getNoteX(), y, g.getNoteX(), y);
+					p.getChildren().add(b);
+					counterQuarterEight++;
+				} else {
+					counterQuarterEight = 0;
+					counter16 = 0;
+				}
 				}
 			}
 
@@ -33,7 +46,7 @@ public class DrawBars {
 			p.getChildren().add(a);
 		}
 	}
-	
+
 	private int getMeasureYLocation(int measureNumber) {
 		Measure m = new Measure(0, p, 0);
 		int measuresY = ((measureNumber-1) / 3)*200 + (m.getSpaceBetweenBarsHorizontal()*5) + 40;
