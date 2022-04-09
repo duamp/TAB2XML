9d3a331dc9e0797995d4355b4320140ac4e368ba
Riffi Manoj <rifleriff@gmail.com>
2022-04-09 04:54:30 -0400

    gtm done

--- src/main/java/GUI/PreviewFileController.java
+++ src/main/java/GUI/PreviewFileController.java
@@ -1,14 +1,18 @@
 package GUI;
 
+import java.awt.BasicStroke;
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.scene.canvas.Canvas;
 
 import org.fxmisc.richtext.CodeArea;
 
 import GUI.settingsGUI;
 import converter.Converter;
+import converter.measure.TabMeasure;
 import drawMusic.DrawBars;
 import drawMusic.DrawDrumsNotes;
 import drawMusic.DrawGuitarNotes;
@@ -20,7 +24,9 @@ import drawMusic.Measure;
 import drawMusic.ParseDrumNotes;
 import drawMusic.ParseGuitarNotes;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
 
 import javafx.fxml.FXMLLoader;
 
@@ -30,13 +36,16 @@ import javafx.print.PageOrientation;
 import javafx.print.Paper;
 import javafx.print.Printer;
 import javafx.print.PrinterJob;
+import javafx.scene.Node;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -49,13 +58,14 @@ import models.ScorePartwise;
 import music_player.PlayerController;
 import music_player.XmlSequence;
 import note_information.DrumInformation;
-import utility.Settings;
+import utility.Range;
 import note_information.*;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequencer;
+import javax.swing.plaf.basic.BasicTextUI.BasicHighlighter;
 
 public class PreviewFileController extends Application {
 
@@ -67,13 +77,15 @@ public class PreviewFileController extends Application {
 	private LinkedList<GuitarInformation> aLGuitar;
 	private String instrument;
 	public Converter converter;
+	
 	@FXML public CodeArea mainText;
 	@FXML private AnchorPane ancorPane;
 	@FXML TextField gotoMeasureField;
 	@FXML private Pane pane;
 	@FXML private Button button;
 	@FXML Button HomeBtn;
-	@FXML private Button Settings;
+	private boolean check = false;
+    Rectangle r = null;
 
 	public int getMeasureNumber() {
 		return sp.getParts().get(0).getMeasures().size();
@@ -102,10 +114,67 @@ public class PreviewFileController extends Application {
 
 	@FXML
 	private void handleGotoMeasure() {
-		int measureNumber = Integer.parseInt(gotoMeasureField.getText() );
+		//int measureNumber = Integer.parseInt(gotoMeasureField.getText() );
+		int measureNumber = Integer.parseInt(gotoMeasureField.getText());
 		System.out.println(measureNumber);
-	}
+		
+		
+		if (!goToMeasure(measureNumber)) {
+			
 
+            		Alert alert = new Alert(Alert.AlertType.ERROR);
+            		
+               alert.setContentText("Measure " + measureNumber + " could not be found.");
+               
+               
+            alert.setHeaderText(null);
+            
+            	alert.show();
+            	
+          
+            	
+    } else {
+            highlight(measureNumber);
+    }
+    
+    // ancorPane.
+    //.....
+    // need to finish go to measure implementation
+}
+		
+		
+		
+		//.....
+	
+
+	
+	
+	
+	
+	
+	
+	private boolean goToMeasure(int measureCount) {
+		
+		
+        TabMeasure measure = converter.getScore().getMeasure(measureCount);
+       
+        System.out.println(measure);
+        if (measure == null)
+        	
+                return false;
+        
+        else
+        	
+                return true;
+}
+	
+	
+	
+	
+	
+	
+	
+	
 	public void startup(ScorePartwise sp, CodeArea mainText, Converter converter) throws IOException{
 		this.mainText = mainText;
 		this.converter = converter;
@@ -157,7 +226,10 @@ public class PreviewFileController extends Application {
 		}
 	}
 
-	public void initialize() {}
+	public void initialize() {
+		
+		
+	}
 
 	@FXML
 	public void saveSSHandle() {
@@ -236,7 +308,7 @@ public class PreviewFileController extends Application {
 		 * 		8. Verdana
 		 * 3. Increase/Decrease Measure Size 
 		 */
-
+		
 		
 	}
 
@@ -250,13 +322,484 @@ public class PreviewFileController extends Application {
 	}
 
 	private void highlight(int index) {
-		int x = aLGuitar.get(index).getNoteX();
-		int y = aLGuitar.get(index).getNoteY();
-		Rectangle r = new Rectangle(x,y,10,10);
-		Color c = Color.CYAN;
-		r.setFill(c);
-		r.setOpacity(0.3);
-		pane.getChildren().add(r);
+		
+		 if (check) {
+             pane.getChildren().remove(r);
+     }
+		 
+		 
+//		int x = aLGuitar.get(index).getNoteX() - 57;
+//		System.out.println("x " + x);
+//		int y = aLGuitar.get(index).getNoteY() - 70;
+//		System.out.println("y " + y);
+//		 r = new Rectangle(x,y,10,10);
+//		
+//		r.setFill(c);
+//		r.setOpacity(0.3);
+//		pane.getChildren().add(r);
+//		check = true;
+		 
+		 
+	 
+		 
+		 double opp = 0.3; // opacity
+		 Color c = Color.CYAN;
+		 
+		 if (index == 1) {
+             pane.getChildren().remove(check);
+             r = new Rectangle(10, 1, 300, 90);
+             r.setFill(c);
+             r.setOpacity(opp);
+             pane.getChildren().add(r);
+             check = true;
+     } else if (index == 2) {
+             pane.getChildren().remove(check);
+             r = new Rectangle(310, 1, 300, 90);
+             r.setFill(c);
+             r.setOpacity(opp);
+             pane.getChildren().add(r);
+             check = true;
+     } else if (index == 3) {
+         r = new Rectangle(610, 1, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 4) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 200, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 5) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 200, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 6) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 200, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 7) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 400, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 8) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 400, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 9) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 400, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 10) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 600, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 11) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 600, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 12) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 600, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 13) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 800, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 14) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 800, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 15) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 800, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         TabMeasure measure = converter.getScore().getMeasure(index);
+         List<Range> linePositions = measure.getRanges();
+         int pos = linePositions.get(0).getStart();
+         mainText.moveTo(pos);
+         mainText.requestFollowCaret();
+         mainText.requestFocus();
+         check = true;
+ } else if (index == 16) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 1000, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 17) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 1000, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 18) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 1000, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         mainText.moveTo(index);
+         
+         check = true;
+ } else if (index == 19) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 1200, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 20) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 1200, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 21) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 1200, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+ 
+         check = true;
+ }else if (index == 22) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 1400, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 23) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 1400, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 24) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 1400, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+ 
+         check = true;
+ }else if (index == 25) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 1600, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 26) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 1600, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 27) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 1600, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+ 
+         check = true;
+ }else if (index == 28) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 1800, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 29) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 1800, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 30) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 1800, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+ 
+         check = true;
+ }else if (index == 31) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 2000, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 32) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 2000, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 33) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 2000, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+ 
+         check = true;
+ }else if (index == 34) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 2200, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 35) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 2200, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 36) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 2200, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+ 
+         check = true;
+ }else if (index == 38) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 2400, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 39) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 2400, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 40) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 2400, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+ 
+         check = true;
+ }else if (index == 41) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 2600, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 42) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 2600, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 43) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 2600, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+ 
+         check = true;
+ }else if (index == 44) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 2800, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 45) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 2800, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 46) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 2800, 300, 90);                 
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r); 
+         check = true;
+ }else if (index == 47) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 3000, 300, 90);                  
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 48) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(310, 3000, 300, 90);                 
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ } else if (index == 49) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(610, 3000, 300, 90);
+         
+         
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ }else if (index == 50) {
+         pane.getChildren().remove(check);
+         r = new Rectangle(10, 3200, 300, 90);          
+         r.setFill(c);
+         r.setOpacity(opp);
+         pane.getChildren().add(r);
+         check = true;
+ }
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
 	}
 
 	@Override
